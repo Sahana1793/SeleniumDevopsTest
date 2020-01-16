9f982d7e37d020d5115e07c62635c261ec6c4d07
Sahana Gopal <SahanaG@spotcable.com>
2020-01-16 16:05:07 -0600

    Initial Commit

---
+++ SeleniumDevops/Class1.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumDevops
+{
+    [TestFixture]
+    public class Class1
+    {
+        [Test]
+        public void FirstTest()
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://www.youtube.com/");
+
+        }
+
+    }
+}
